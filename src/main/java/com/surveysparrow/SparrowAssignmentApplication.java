800b159f24d39113092a80f03c84d32dd0795721
malayjoshi <malayace.joshi@gmail.com>
2021-03-22 13:59:56 +0530

    init commit

---
+++ src/main/java/com/surveysparrow/SparrowAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.surveysparrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparrowAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparrowAssignmentApplication.class, args);
+	}
+
+}
